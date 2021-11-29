a0c4e6a7410b97df9c13dbb22b62cdab7477a114
Claudio Sicari <claudiosicari99@gmail.com>
2021-11-29 17:48:07 +0100

    v1.0 bot Discord

--- bot_commands/general.py
+++ bot_commands/general.py
@@ -8,18 +8,25 @@ class General(commands.Cog):
 
     # Annotation to specify the decorator (Decorator Pattern)
     # The function command() contains another function called decorator(func) and returns it
-    @commands.command()
+    # During run-time, the method goldbet (or bwin) is passed to the decorator, through the command function,
+    # who instantiates a Command class into a variable with the same name of the function passed to it
+    @commands.command(brief='Shows the quotes of the principal soccer matches inside GoldBet website',
+                      description='This command shows the most important quotes about all the soccer matches inside'
+                                  ' GoldBet website. The quotes are 1x2 and Under/Over')
     # This is the function passed by param to the decorator -> decorator(func)
-    async def call_scratch(self, context:Context, msg:str, category:str):
-        if msg == 'goldbet':
-            # The variable result is our generator. This generator contains string elements that will be showed
-            # in the chat
-            result = goldbet.run(category)
-        else:
-            result = bwin.run(category)
+    async def goldbet(self, context:Context, category:str):
+        # The variable result is our generator. This generator contains string elements that will be showed
+        # in the chat
+        result = '\n'.join(goldbet.run(category))
 
-        for element in result:
-            # await is a command similar to return but for async functions
-            # await is necessary when a context switch happens (for example when two command are invoked simultaneously)
-            # the funct stops and resumes his work after the other event finishes
-            await context.send(element)
+        # await is a command similar to return but for async functions
+        # await is necessary when a context switch happens (for example when two command are invoked simultaneously)
+        # the funct stops and resumes his work after the other event finishes
+        await context.send(result)
+
+    @commands.command(brief='Shows the quotes of the principal soccer matches inside Bwin website',
+                      description='This command shows the most important quotes about all the soccer matches inside bwin'
+                                  ' website. The quotes are 1x2 and Over/Under')
+    async def bwin(self, ctx:Context, category:str):
+        result = '\n'.join(bwin.run(category))
+        await ctx.send(result)
